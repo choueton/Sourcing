91f06704227e7cb8549ff48ccc1cd44c16bb69d2
choueton <sanogoprince565@gmail.com>
2023-06-15 09:27:24 +0000

    b

--- app.py
+++ app.py
@@ -36,76 +36,6 @@ def add_from_promo():
     return render_template('add_from_promo.html', formation=formation, bailleur=bailleur)
 
 ############################################################
-
-@app.route('/promo')
-def list_promo():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT promo.id, promo.nom_promo, bailleur.nom_bailleur, formation.nom_formation, promo.debut_promo, promo.fin_promo FROM bailleur JOIN promo ON bailleur.id = promo.id_bailleur JOIN formation ON formation.id = promo.id_formation")
-    data = cur.fetchall()
-    cur.close()
-    return render_template('list_promo.html',promo=data)
-
-@app.route('/add_promo', methods=["POST"])
-def add_promo():
-
-    # Obtenir les données du formulaire
-    nom_promo = request.form['nom_promo']
-    id_bailleur = request.form['id_bailleur']
-    id_formation = request.form['id_formation']
-    debut_promo = request.form['debut_promo']
-    fin_promo = request.form['fin_promo']
-
-    # Établir une connexion à la base de données
-    cur = mysql.connection.cursor()
-
-    # Insérer les données dans la table 'promo'
-    cur.execute("INSERT INTO promo (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo) VALUES (%s, %s, %s, %s, %s)", (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo))
-
-    # Valider la transaction
-    mysql.connection.commit()
-
-    # Fermer la connexion à la base de données
-    cur.close()
-
-    # Rediriger vers la page 'promo'
-    return redirect(url_for('list_promo'))
-
-@app.route('/upd_promo/<int:id>', methods=["GET","POST"])
-def upd_promo(id):
-
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT promo.id, promo.nom_promo, bailleur.nom_bailleur, formation.nom_formation, promo.debut_promo, promo.fin_promo FROM bailleur JOIN promo ON bailleur.id = promo.id_bailleur JOIN formation ON formation.id = promo.id_formation WHERE promo.id=%s", (id,))
-    # cur.execute("SELECT * FROM promo WHERE id=%s", (id,))
-    promo_info = cur.fetchall()
-    cur.execute("SELECT * FROM bailleur")
-    bailleur = cur.fetchall()
-    cur.execute("SELECT * FROM formation")
-    formation = cur.fetchall()
-    cur.close()
-
-    if request.method == "POST":
-
-        nom_promo = request.form['nom_promo']
-        id_bailleur = request.form['id_bailleur']
-        id_formation = request.form['id_formation']
-        debut_promo = request.form['debut_promo']
-        fin_promo = request.form['fin_promo']
-
-        cur = mysql.connection.cursor()
-        cur.execute("UPDATE promo SET nom_promo=%s, id_bailleur=%s, id_formation=%s, debut_promo=%s, fin_promo=%s WHERE id=%s", (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo, id))
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('list_promo'))
-    
-    return render_template('upd_promo.html',promo_info=promo_info, bailleur=bailleur,formation=formation)
-
-@app.route('/delete_promo/<string:id_data>', methods=["GET"])
-def delete_promo(id_data):
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM promo WHERE id=%s", [id_data])
-    mysql.connection.commit()
-    cur.close()
-    return redirect(url_for('list_promo'))
 ################################################################
 
 @app.route('/')
@@ -238,5 +168,78 @@ def delete_formation(id_data):
     return redirect(url_for('list_formation'))
 
 ######################################################################################################################
+
+# promo
+@app.route('/promo')
+def list_promo():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT promo.id, promo.nom_promo, bailleur.nom_bailleur, formation.nom_formation, promo.debut_promo, promo.fin_promo FROM bailleur JOIN promo ON bailleur.id = promo.id_bailleur JOIN formation ON formation.id = promo.id_formation")
+    data = cur.fetchall()
+    cur.close()
+    return render_template('list_promo.html',promo=data)
+
+@app.route('/add_promo', methods=["POST"])
+def add_promo():
+
+    # Obtenir les données du formulaire
+    nom_promo = request.form['nom_promo']
+    id_bailleur = request.form['id_bailleur']
+    id_formation = request.form['id_formation']
+    debut_promo = request.form['debut_promo']
+    fin_promo = request.form['fin_promo']
+
+    # Établir une connexion à la base de données
+    cur = mysql.connection.cursor()
+
+    # Insérer les données dans la table 'promo'
+    cur.execute("INSERT INTO promo (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo) VALUES (%s, %s, %s, %s, %s)", (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo))
+
+    # Valider la transaction
+    mysql.connection.commit()
+
+    # Fermer la connexion à la base de données
+    cur.close()
+
+    # Rediriger vers la page 'promo'
+    return redirect(url_for('list_promo'))
+
+@app.route('/upd_promo/<int:id>', methods=["GET","POST"])
+def upd_promo(id):
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT promo.id, promo.nom_promo, bailleur.nom_bailleur, formation.nom_formation, promo.debut_promo, promo.fin_promo FROM bailleur JOIN promo ON bailleur.id = promo.id_bailleur JOIN formation ON formation.id = promo.id_formation WHERE promo.id=%s", (id,))
+    # cur.execute("SELECT * FROM promo WHERE id=%s", (id,))
+    promo_info = cur.fetchall()
+    cur.execute("SELECT * FROM bailleur")
+    bailleur = cur.fetchall()
+    cur.execute("SELECT * FROM formation")
+    formation = cur.fetchall()
+    cur.close()
+
+    if request.method == "POST":
+
+        nom_promo = request.form['nom_promo']
+        id_bailleur = request.form['id_bailleur']
+        id_formation = request.form['id_formation']
+        debut_promo = request.form['debut_promo']
+        fin_promo = request.form['fin_promo']
+
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE promo SET nom_promo=%s, id_bailleur=%s, id_formation=%s, debut_promo=%s, fin_promo=%s WHERE id=%s", (nom_promo, id_bailleur, id_formation, debut_promo, fin_promo, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('list_promo'))
+    
+    return render_template('upd_promo.html',promo_info=promo_info, bailleur=bailleur,formation=formation)
+
+@app.route('/delete_promo/<string:id_data>', methods=["GET"])
+def delete_promo(id_data):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM promo WHERE id=%s", [id_data])
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('list_promo'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
